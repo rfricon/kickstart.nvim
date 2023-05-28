5029d5ad11aa819ca728c615b845a98f83c0f32a
Romain FRICON <romain.fricon@gmail.com>
2023-05-28 11:56:08 +0200

    fix Github Copilot + neo-tree

--- after/plugin/defaults.lua
+++ after/plugin/defaults.lua
@@ -6,3 +6,7 @@ vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
 vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
 vim.keymap.set('n', '<C-p>', builtin.git_files, {})
+
+-- Copilot
+vim.g.copilot_no_tab_map = true
+vim.api.nvim_set_keymap("i", "<C-J>", 'copilot#Accept("<CR>")', { silent = true, expr = true })
